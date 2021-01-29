ae0a92db3a5beaa066e176e8aa68c1efff31bdfa
Luiz Fernando <lyiz.feh12rodrigues@gmail.com>
2021-01-29 13:39:36 -0300

    Subindo banco de dados

--- app/src/main/java/com/example/tihasg/dao/AlunoDAO.java
+++ app/src/main/java/com/example/tihasg/dao/AlunoDAO.java
@@ -1,4 +1,22 @@
 package com.example.tihasg.dao;
 
-public class AlunoDAO {
+import com.example.tihasg.model.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class    AlunoDAO {
+
+    private final static List<Aluno> alunos = new ArrayList<>();
+    public List<Aluno> todos;
+
+    public void salva(Aluno aluno) {
+        alunos.add(aluno);
+
+
+    }
+
+    public List<Aluno> todos() {
+        return new ArrayList<>(alunos);
+    }
 }
